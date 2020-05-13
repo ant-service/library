cda1d09110b9e7434feed5f59e91091f40996906
马海搏 <tabhaibo@163.com>
2020-05-13 16:03:21 +0800

    dev

--- source/common/Config.php
+++ source/common/Config.php
@@ -1,16 +1,19 @@
 <?php
 
-use AntService\Src\Common\DataType;
+namespace AntService\Src\Common;
+
+use AntService\OutPut;
 
 class Config
 {
     public static function read(string $configName)
     {
+        var_dump($_SERVER['DOCUMENT_ROOT']);exit();
         $keyArray = explode('.', $configName);
         $filePath = $_SERVER['DOCUMENT_ROOT'] . 'config/' . array_shift($keyArray) . '.json';
         $configContent = $GLOBALS['config_' . $filePath] ?? null;
         if (!is_file($filePath)) {
-            return Output::error('READ_CONFIG_FAIL', '读取配置文件失败,请检查配置文件[' . $filePath . ']是否存在');
+            return OutPut::error('READ_CONFIG_FAIL', '读取配置文件失败,请检查配置文件[' . $filePath . ']是否存在');
         }
         if ($configContent === null) {
             $configContent = DataType::convertArray(file_get_contents($filePath));
