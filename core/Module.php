c6bdf70879527f3e54d3b03024cc061df788e288
马海搏 <tabhaibo@163.com>
2020-05-15 10:08:06 +0800

    dev

--- core/Module.php
+++ core/Module.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AntService;
+
 use AntService\Src\Common\Config;
 
 class Module
@@ -10,27 +11,28 @@ class Module
     public static function use(string $moduleName, array $arguments = array())
     {
         $userDir = self::getUserDir();
-        self::setArguments($moduleName,$arguments);
-        
+        $moduleName = strtoupper($moduleName);
+        self::setArguments($moduleName, $arguments);
+
         $moduleFile = $userDir . 'module/' . $moduleName;
 
-        if(is_file($moduleFile)){
+        if (is_file($moduleFile)) {
             $isDownload = false;
-        }else{
+        } else {
             $isDownload = true;
             self::download($moduleName, $moduleFile);
         }
-        
+
         require_once $moduleFile;
 
         if (!class_exists($moduleName)) {
-            Output::error('VERIFY_CLASS_FAIL', '验证模块类失败,该模块[' . $moduleName . ']未实现同名类');
+            OutPut::error('VERIFY_CLASS_FAIL', '验证模块类失败,该模块[' . $moduleName . ']未实现同名类');
         }
         $isDownload and self::firstLoad($moduleName);
         return new $moduleName();
     }
 
-    private static function setArguments($moduleName,$arguments)
+    private static function setArguments($moduleName, $arguments)
     {
         self::$arguments[$moduleName] = $arguments;
     }
@@ -55,7 +57,7 @@ class Module
         $dbDepend = $moduleName::$dbDepend ?? array();
         $dataBaseModule = Config::readEnv('DATABASE_MODULE');
         $dataBaseModule !== $moduleName && $dbDepend != null
-            and self::use($dataBaseModule)->syncDataBase($dbDepend) or Output::errorOutput('SYNC_DATABASE_FAIL', '同步数据库结构失败');
+            and self::use($dataBaseModule)->syncDataBase($dbDepend) or OutPut::error('SYNC_DATABASE_FAIL', '同步数据库结构失败');
 
         //模块初始化操作
         is_callable([$moduleName, 'init']) and self::use($moduleName)::init();
@@ -73,18 +75,18 @@ class Module
         $downloadUrl = 'compress.zlib://' . Config::readEnv('SERVICE_URL') . '/module/' . $moduleName . '.msphp';
         $result = file_get_contents($downloadUrl);
         if (!$result) {
-            Output::error('DOWNLOAD_MODULE_FAIL', '官方模块库中不存在此模块');
+            OutPut::error('DOWNLOAD_MODULE_FAIL', '官方模块库中不存在此模块');
         }
         if (!file_put_contents($moduleFile, $result)) {
-            Output::error('WRITE_MODULE_FAIL', '写入模块失败,请检查是否拥有写入权限[' . $moduleFile . ']');
+            OutPut::error('WRITE_MODULE_FAIL', '写入模块失败,请检查是否拥有写入权限[' . $moduleFile . ']');
         }
     }
 
-    private static function getUserDir(){
-        if(function_exists('getUserCustomDir')){
+    private static function getUserDir()
+    {
+        if (function_exists('getUserCustomDir')) {
             return getUserCustomDir();
         }
         return getUserDir();
-
     }
 }
